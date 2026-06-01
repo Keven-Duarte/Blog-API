4d8fb9d4f2ddaae94edf2249ef16b7229521c5fe
Keven-Duarte <Kevenoliveiraduarte2020@gmail.com>
2026-06-01 08:35:20 -0300

    criando endpoints

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,13 @@
+import { RequestHandler } from "express";
+
+export const signIn: RequestHandler = (req, res) => {
+    // Lógica de autenticação
+};
+
+export const signUp: RequestHandler = (req, res) => {
+    // Lógica de registro de um novo usuário
+};
+
+export const validate: RequestHandler = (req, res) => {
+    // Lógica de validação de token de autenticação
+};
